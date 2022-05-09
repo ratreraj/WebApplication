579f3dc7ae3694a933cf902de0411756a82cf47f
raj ratre <ratre.raj@gmail.com>
2022-05-09 08:05:14 +0530

    intial commit

---
+++ WebApplication/Controllers/EmployeeController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using WebApplication.Models;
+using System.Text.Json;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using System;
+
+namespace WebApplication.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = TempData["Employee"];
+            TempData.Keep("Employee");
+
+            var data2 = TempData.Peek("Employee");
+           
+
+
+            EmployeeModel employeeModel = JsonSerializer.Deserialize<EmployeeModel>(data.ToString());
+            List<EmployeeModel> employees = new List<EmployeeModel>();
+            employees.Add(employeeModel);
+            EmployeeModel employeeModel2 = JsonSerializer.Deserialize<EmployeeModel>(HttpContext.Session.GetString("emp"));
+
+
+            return View(employees);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(EmployeeModel employeeModel)
+        {
+
+
+            TempData["Employee"] = JsonSerializer.Serialize(employeeModel);
+            TempData["Message"]=  "Hello Guys wellcome in my class";
+            string msg = "Hi Welcome my class";
+            HttpContext.Session.SetString("Msg", msg);
+            HttpContext.Session.SetString("emp", JsonSerializer.Serialize(employeeModel));
+
+            Response.Cookies.Append("non-persistent", "my non persistent cookies");
+            Response.Cookies.Append("persistent", "my persistent cookies", new CookieOptions { Expires= DateTime.Now.AddDays(1) });
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ WebApplication/Models/EmployeeModel.cs
@@ -0,0 +1,17 @@
+﻿namespace WebApplication.Models
+{
+    public class EmployeeModel
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string Address { get; set; }
+        public string State { get; set; }
+        public string City { get; set; }
+        public string Mobile { get; set; }
+        public string Email { get; set; }
+        public bool Terms { get; set; }
+
+    }
+}
